3d92fe3eaf4f7687cfb2ee72188dfed967d01f5e
stevencodeblooded <steven.ochieng432@gmail.com>
2025-04-03 00:14:36 +0300

    updated callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,13 +1,17 @@
 // api/callback.js
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
 const app = express();
 
 // Enable CORS for all routes
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Root route for health check
+app.get('/', (req, res) => {
+  res.status(200).json({ status: 'ok', message: 'Paystack callback API is running' });
+});
 
 // Paystack verification endpoint
 app.get('/verify-payment', async (req, res) => {
@@ -80,5 +84,5 @@ app.get('/status', (req, res) => {
   res.status(200).json({ status: 'ok', message: 'Paystack callback API is running' });
 });
 
-// Export the Express API as default
+// For serverless environment, we need to export the Express app
 module.exports = app;
